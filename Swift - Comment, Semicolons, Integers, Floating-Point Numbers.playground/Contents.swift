33cb1406b2b1424551b86dae3359ba3faccf2891
Terrick Mansur <terrickmansur@gmail.com>
2019-06-15 18:17:26 +0200

    Add files via upload

---
+++ Swift - Comment, Semicolons, Integers, Floating-Point Numbers.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+/*
+ Swift Variables
+ Date: June 15th 2019
+ Author: Terrick Mansur
+ */
+
+// Single-line commenting
+// let lineOfCode = 0
+
+// Multiline commenting
+/*
+let lineOfCode1 = 1
+let lineOfCode2 = 2
+*/
+
+// Nested multiline commenting
+/*
+/*
+ This function prints "Something"
+ */
+func printsSomething() {
+    print("Something")
+}
+
+let lineOfCodeBelowFunc = "Heyoww"
+*/
+
+// Semicolos
+let line1 = 0; let line2 = 9
+
+// Int bit forms type, 8, 16, 32, 64
+let int8: Int8 = 8
+let int16: Int16 = 16
+let int32: Int32 = 32
+let int64: Int64 = 64
+
+// Int will take the form of the platform 32, or 64
+let normalInt: Int = 0
+
+// UInt bit forms 8, 16, 32, 64
+let uInt: UInt = 8
+let uInt8: UInt8 = 8
+let uInt16: UInt16 = 16
+let uInt32: UInt32 = 32
+let uInt64: UInt64 = 64
+
+// Int bounds
+let min = UInt64.min
+let max = UInt64.max
+
+// Floating bit forms 32, 64, 80
+let floatingValue32: Float32 = 32
+let floatingValue64: Float64 = 64
+let floatingValue80: Float80 = 80
+
+// Float is 32 bit Float
+let normalFloat: Float = 32
+
+// Double is basically a 64 bit Float
+let doubleValue: Double = 0.0
